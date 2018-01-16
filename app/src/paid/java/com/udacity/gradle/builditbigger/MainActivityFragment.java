5399baf58b3d5a17b308f6efb8d0a9b018b625d0
Ivan Zhang <ivanzhangemail@gmail.com>
2018-01-16 05:51:43 -0500

    Source Sets and freeCompile
    * Implemented source sets using a free and a paid directory
    
    * Added the freeCompile to compile only for the free flavor

--- app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -6,10 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
-
-
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -22,14 +18,6 @@ public class MainActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        boolean isPaid = getContext().getResources().getBoolean(R.bool.isPaid);
-        if(!isPaid){
-            AdView mAdView = root.findViewById(R.id.adView);
-            AdRequest adRequest = new AdRequest.Builder()
-                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                    .build();
-            mAdView.loadAd(adRequest);
-        }
 
         return root;
     }
